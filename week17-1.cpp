a07c81a2fc71618bc3fa289b482687ef8393bc06
賴奕廷 <91170853+s913378@users.noreply.github.com>
2022-01-05 11:37:25 +0800

    Add files via upload

---
+++ week17-1.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n=20221991;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    n=n/10;
+    printf("%d => %d %d\n",n,n/10,n%10);
+    return 0;
+}
